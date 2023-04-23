7fc538dcac6ecde9a4f34fe314b19d71b8d1c970
garalapallil <vedasri@gmail.com~^c>
2023-04-23 15:34:17 +0530

    adding 2nd file

---
+++ v1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main();
+{
+int a[100][100],n,i,j,count;
+count=0;
+printf("enter the size of 2d array");
+scanf("%d",&n);
+printf("enter the elements of 2d array");
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i][j]);
+}
+}
+for(i=0;i<n;i++);
+{
+for(j=0;j<n;j++);
+if(a[i][j]=1)
+count++;
+}
+}
+printf("no.of pairs of friends=%d",count/2);
+return 0,
+}
